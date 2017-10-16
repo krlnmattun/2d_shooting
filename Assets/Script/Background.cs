93f8e2202c768936e9e85637a09be6d23a08c68b
krlnmattun <krlnmattun@gmail.com>
2017-10-16 20:33:23 +0900

    チュートリアル 背景～Wave

---
+++ Assets/Script/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+    // スクロール速度 /
+    public float speed = 0.1f;
+
+	// Update is called once per frame
+	void Update () {
+		// 時間経過でY座標を1まで変化(0～1をループ) /
+        float y = Mathf.Repeat(Time.time * speed, 1);
+
+        // Y座標のオフセット /
+        Vector2 offset = new Vector2(0, y);
+
+        // マテリアルにオフセットを設定 /
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+	}
+}
---
+++ Assets/Script/Emitter.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour {
+
+    // Wave prefabの格納 /
+    public GameObject[] waves;
+
+    // 現在のWave /
+    private int currentWave;
+
+    IEnumerator Start() {
+        // Waveが存在しない場合はコルーチン終了 /
+        if (waves.Length == 0) {
+            yield break;
+         }
+
+        while (true) {
+            // Wave作成 /
+            GameObject wave = (GameObject)Instantiate(waves[currentWave], transform.position, Quaternion.identity);
+
+            // WaveをEmitterの子要素にする /
+            wave.transform.parent = transform;
+
+            // Waveの子要素のEmenyが全削除されるまで待機 /
+            while (wave.transform.childCount != 0) {
+                yield return new WaitForEndOfFrame();
+            }
+
+            // Wave削除 /
+            Destroy(wave);
+
+            // 格納Waveを全て実行したらWave数をリセットする /
+            if (waves.Length <= ++currentWave) {
+                currentWave = 0;
+            }
+        }
+    }
+}
